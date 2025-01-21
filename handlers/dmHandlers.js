2d1b54f06d3cd262c16654acf13d7b61c3ec79f1
rayhanalmim <rayhanalmim1@gmail.com>
2025-01-21 13:24:06 +0600

    feat: refactor direct message handling to use conversation ID and improve error logging

--- handlers/dmHandlers.js
+++ handlers/dmHandlers.js
@@ -1,53 +1,43 @@
-import mongoose from 'mongoose';
-import Message from '#models/messages/messagesModel.js';
-import { createConversationId, fetchMessages, handleError } from './utils.js';
+import mongoose from "mongoose";
+import Message from "#models/messages/messagesModel.js";
+import { createConversationId, fetchMessages, handleError } from "./utils.js";
 
 export const handleDMEvents = (socket, anthillChat) => {
-  socket.on('join_dm', async ({ senderId, recipientId }) => {
+  socket.on("join_dm", async ({ conversationId }) => {
     try {
-      if (
-        !mongoose.Types.ObjectId.isValid(senderId) ||
-        !mongoose.Types.ObjectId.isValid(recipientId)
-      ) {
-        throw new Error('Invalid user IDs');
-      }
-
-      const conversationId = createConversationId(senderId, recipientId);
-
       socket.join(conversationId);
 
+      console.log("user joined dm", conversationId);
       const messages = await fetchMessages({ conversationId });
-      socket.emit('private_message_history', messages);
+      console.log("messages", messages);
+      socket.emit("private_message_history", messages);
     } catch (error) {
-      handleError(socket, error, 'Failed to join the private conversation');
+      handleError(socket, error, "Failed to join the private conversation");
     }
   });
 
-  socket.on('send_dm', async ({ senderId, recipientId, content }) => {
+  socket.on("send_dm", async ({ senderId, recipientId, content }) => {
     try {
       if (
         !mongoose.Types.ObjectId.isValid(senderId) ||
         !mongoose.Types.ObjectId.isValid(recipientId)
       ) {
-        throw new Error('Invalid user IDs');
+        throw new Error("Invalid user IDs");
       }
-
       const conversationId = createConversationId(senderId, recipientId);
-
       const message = new Message({
         senderId,
         recipientId,
         content,
-        messageType: 'text',
+        messageType: "text",
         conversationId,
       });
 
       await message.save();
-      const populatedMessage = await message.populate('senderId');
-
-      anthillChat.to(conversationId).emit('send_dm', populatedMessage);
+      anthillChat.to(conversationId).emit("recived_dm", message);
     } catch (error) {
-      handleError(socket, error, 'Failed to send the direct message');
+      console.log(error);
+      handleError(socket, error, "Failed to send the direct message");
     }
   });
 };
